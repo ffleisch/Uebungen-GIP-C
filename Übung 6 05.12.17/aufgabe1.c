0edd8086419d7eac007fd765934feda0d03b6058
FFleisch <Felix.Fleisch@t-online.de>
2017-12-05 14:46:07 +0100

    aufgabe 1 uebung 6 fertig

---
+++ Übung 6 05.12.17/aufgabe1.c
@@ -0,0 +1,28 @@
+//aufgabe1.c
+//Übung 6 Aufgabe 1 GIP 
+//Felix Fleisch Gruppe Die. 14-18 170945
+
+#include<stdio.h>
+#include<math.h>
+
+double zinsFormel(double k,double p,double n){	
+	return(k*pow(p+1,n));
+}
+
+int main(){
+	double k,p,n;
+	printf("Bitte Kapital eingeben: \n");
+	scanf("%lf",&k);
+	printf("Bitte Zinssatz eingeben: \n");
+	scanf("%lf",&p);
+	printf("Bitte Laufzeit eingeben: \n");
+	scanf("%lf",&n);
+
+	p/=100;	
+
+	for(int i=1;i<=n;i++){
+		printf("Jahr: %d Kapital: %.2f\n",i,zinsFormel(k,p,i));
+	}
+
+	return(0);
+}
